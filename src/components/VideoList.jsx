f146f5b5e11750f9309cd7cb39d937871c2e853a
ericletsgo <ericletsgo@gmail.com>
2018-05-09 18:19:42 -0700

    added functionality for video player and video list

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,17 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
+var VideoList = (props) => (
+  //<div className="video-list">
+    // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+
+  //</div>
+    <div className="video-list">
+      {props.videos.map(video =>
+        <VideoListEntry video={video} />  
+      )}
+    </div>
 );
+
+// ReactDOM.render(<videoListEntry />, document.getElementById("list"));
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
